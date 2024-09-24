e24dc21a63706d4656d05da0f9222f3c1082936e
veryCrunchy <me@verycrunchy.dev>
2024-09-24 05:34:04 +0200

    wip: improve setup, handle ip host and ssl config

--- setup.sh
+++ setup.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-# Define the default values
 HULY_VERSION="v0.6.295"
 DOCKER_NAME="huly"
 CONFIG_FILE="huly.conf"
 
-
 if [ -f "$CONFIG_FILE" ]; then
     source "$CONFIG_FILE"
 fi
-clear
 
 while true; do
-    read -p "Enter the host address (domain name or IP) [default: ${HOST_ADDRESS:-localhost}]: " input
-    _HOST_ADDRESS="${input:-${HOST_ADDRESS:-localhost}}"
-    #TODO: proper validation
-    #if [[ "$_HOST_ADDRESS" =~ ^[a-zA-Z0-9.-]+$ || "$_HOST_ADDRESS" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    read -p "Enter the host address (domain name or IP) [default: ${HOST_ADDRESS:-<IP>}]: " input
+    _HOST_ADDRESS="${input:-${HOST_ADDRESS:-$(hostname -I | awk '{print $1}')}}"
+    # TODO: proper validation
+    # if [[ "$_HOST_ADDRESS" =~ ^[a-zA-Z0-9.-]+$ || "$_HOST_ADDRESS" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
         break
-    #else
-    #   echo "Invalid host address. Please enter a valid domain name or IP."
-    #fi
+    # else
+    #    echo "Invalid host address. Please enter a valid domain name or IP."
+    # fi
 done
-clear
+
 
 while true; do
     read -p "Enter the port for HTTP [default: ${HTTP_PORT:-80}]: " input
@@ -32,19 +29,26 @@ while true; do
         echo "Invalid port. Please enter a number between 1 and 65535."
     fi
 done
-clear
-# TODO: true/false
-while true; do
-    read -p "Will you serve Huly over SSL? (y/N) [default: ${SECURE}]: " SECURE_INPUT
-    case "$SECURE_INPUT" in
-        [Yy]* )
-            SECURE="s";break;;
-        [Nn]* )
-            SECURE="";break;;
-        * )
-            echo "Invalid input. Please enter Y or N.";;
-    esac
-done
+
+if [[ "$_HOST_ADDRESS" == "localhost" || "$_HOST_ADDRESS" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    _HOST_ADDRESS="${_HOST_ADDRESS}:\${HTTP_PORT}"
+    SECURE=""
+else
+    while true; do
+        read -p "Will you serve Huly over SSL? (y/N) [default: ${SECURE}]: " input
+        case "${input}" in
+            [Yy]* )
+                _SECURE="true"; break;;
+            [Nn]* )
+                _SECURE=""; break;;
+            "" )
+                _SECURE="${SECURE:+true}"; break;;
+            * )
+                echo "Invalid input. Please enter Y or N.";;
+        esac
+    done
+fi
+
 clear
 
 SECRET=false
@@ -60,16 +64,21 @@ else
   echo "Run this script with --secret to generate a new secret."
 fi
 
-
-export SECURE
+export SECURE=$_SECURE
 export HOST_ADDRESS=$_HOST_ADDRESS
 export HTTP_PORT=$_HTTP_PORT
 export HULY_SECRET=$(cat huly.secret)
 
 envsubst < template.huly.conf > $CONFIG_FILE
 
-echo -e "\033[1;32mSetup is complete!\033[0m"
-
+echo -e "\n\033[1;34mConfiguration Summary:\033[0m"
+echo -e "Host Address: \033[1;32m$_HOST_ADDRESS\033[0m"
+echo -e "HTTP Port: \033[1;32m$_HTTP_PORT\033[0m"
+if [[ -n "$SECURE" ]]; then
+    echo -e "SSL Enabled: \033[1;32mYes\033[0m"
+else
+    echo -e "SSL Enabled: \033[1;31mNo\033[0m"
+fi
 read -p "Do you want to run 'docker compose up -d' now to start Huly? (Y/n): " RUN_DOCKER
 case "${RUN_DOCKER:-Y}" in  
     [Yy]* )  
@@ -79,5 +88,6 @@ case "${RUN_DOCKER:-Y}" in
     [Nn]* )
         echo "You can run 'docker compose up -d' later to start Huly."
         ;;
-   
 esac
+
+echo -e "\033[1;32mSetup is complete!\033[0m"
